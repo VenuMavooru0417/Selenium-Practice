2f5818fd3f36e7cd3eb0e8840bdf9878632ddb79
Lenovo <venumavooru0417@gmail.com>
2025-08-06 17:10:51 +0530

    Welcome Test1

--- src/main/java/org/openMrs/WelcomePageTest.java
+++ src/main/java/org/openMrs/WelcomePageTest.java
@@ -1,7 +1,22 @@
 package org.openMrs;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class WelcomePageTest {
     public static void main(String[] args) {
         System.out.println("Hello World....");
+        // Set the system property for the ChromeDriver executable
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/Driver/CD139v.exe"); // Example path for Windows
+
+        // Initialize the ChromeDriver
+        WebDriver driver = new ChromeDriver();
+
+        // Perform automation tasks
+        driver.get("https://www.google.com");
+        System.out.println("Page title: " + driver.getTitle());
+
+        // Close the browser
+        driver.quit();
     }
 }
