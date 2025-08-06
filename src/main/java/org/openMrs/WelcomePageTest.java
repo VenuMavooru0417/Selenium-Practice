dd4927b8e7b7cf54b18170fd1256032008248dd6
Lenovo <venumavooru0417@gmail.com>
2025-08-06 17:48:27 +0530

    Welcome Test2

--- src/main/java/org/openMrs/WelcomePageTest.java
+++ src/main/java/org/openMrs/WelcomePageTest.java
@@ -1,6 +1,8 @@
 package org.openMrs;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WelcomePageTest {
@@ -13,10 +15,17 @@ public class WelcomePageTest {
         WebDriver driver = new ChromeDriver();
 
         // Perform automation tasks
-        driver.get("https://www.google.com");
-        System.out.println("Page title: " + driver.getTitle());
+        driver.get("https://demoqa.com/");
 
-        // Close the browser
-        driver.quit();
+        String act_Title = driver.getTitle();
+        System.out.printf(act_Title);
+
+        String expectedTitle= "DEMOQA";
+        if(act_Title.equals(expectedTitle)) {
+            System.out.println("Test script passed");
+        }else {
+            System.out.println("Test script failed");
+        }
+        driver.close();
     }
 }
